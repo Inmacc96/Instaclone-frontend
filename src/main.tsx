b62c2452956e487d31c7bf19e624fd6c8aa90ad1
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-09 19:12:00 +0100

    Delete react.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'semantic-ui-css/semantic.min.css'
-import './index.scss'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
