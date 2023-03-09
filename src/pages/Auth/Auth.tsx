c36f064c5586914a822c791a63a7eee73c255034
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-09 20:11:47 +0100

    feat: layout auth page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,7 +1,34 @@
+import { Container, Image } from "semantic-ui-react";
+import instaclone from "../../assets/png/instaclone.png";
 import "./Auth.scss";
+import { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <Container fluid className="auth">
+      <Image src={instaclone} />
+
+      <div className="container-form">
+        {showLogin ? <p>Login form</p> : <p>Sign in form</p>}
+      </div>
+
+      <div className="change-form">
+        {showLogin ? (
+          <p>
+            Don't have an account?
+            <span onClick={() => setShowLogin(!showLogin)}>Sign up</span>
+          </p>
+        ) : (
+          <p>
+            Log in with your account!
+            <span onClick={() => setShowLogin(!showLogin)}>Log in</span>
+          </p>
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default Auth;
