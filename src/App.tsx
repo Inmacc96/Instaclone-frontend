b5559b226e355b9f96f97017a85f50594e68d407
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-09 19:39:04 +0100

    feat: split the authentication and guest area

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import { useState } from "react";
 import { ApolloProvider } from "@apollo/client";
 import { Button } from "semantic-ui-react";
 import client from "./config/apollo";
+import Auth from "./pages/Auth";
 
 const App = () => {
+  const [auth, setAuth] = useState(undefined);
+
   return (
     <ApolloProvider client={client}>
-      <div className="app">
-        <h1>Estamos en App</h1>
-        <Button primary>Primary</Button>
-        <Button secondary>Secondary</Button>
-      </div>
+      {auth ? <h1>Authenticated</h1> : <Auth />}
     </ApolloProvider>
   );
 };
