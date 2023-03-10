1833d3a462d872edda6417bf07f8a55a6f11d5ec
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-10 17:27:21 +0100

    feat: add formik to handle SignUpForm

--- src/components/Auth/SignUpForm/SignUpForm.tsx
+++ src/components/Auth/SignUpForm/SignUpForm.tsx
@@ -1,46 +1,71 @@
+import { useFormik } from "formik";
 import { Form, Button } from "semantic-ui-react";
+import { newUser } from "../../../types/auth";
 import "./SignUpForm.scss";
 
+
 const SignUpForm = () => {
-  const handleSubmit = () => {
-    console.log("form submitted");
+  const initialValues: newUser = {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    repeatpassword: "",
   };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: null,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   return (
     <>
       <h2 className="signup-form-title">
         Sign up to see photos and videos of your friends
       </h2>
-      <Form className="signup-form" onSubmit={handleSubmit}>
+      <Form className="signup-form" onSubmit={formik.handleSubmit}>
         <Form.Input
           type="text"
           name="name"
           placeholder="Name and surname"
           autoComplete="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
         />
         <Form.Input
           type="text"
           name="username"
           placeholder="User name"
           autoComplete="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
         />
         <Form.Input
           type="text"
           name="email"
           placeholder="Email"
           autoComplete="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
         />
         <Form.Input
           type="password"
           name="password"
           placeholder="Password"
           autoComplete="new-password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
         <Form.Input
           type="password"
           name="repeatpassword"
           placeholder="Repeat Password"
           autoComplete="new-password"
+          value={formik.values.repeatpassword}
+          onChange={formik.handleChange}
         />
         <Button type="submit" className="btn-submit">
           Sign up
---
+++ src/types/auth.ts
@@ -0,0 +1,7 @@
+export interface newUser {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  repeatpassword: string;
+}
