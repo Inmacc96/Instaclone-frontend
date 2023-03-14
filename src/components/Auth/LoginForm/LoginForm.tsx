b78791730714345f415e288f906bb47fecd70cba
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-14 14:21:01 +0100

    feat: add validation to LoginForm

--- src/components/Auth/LoginForm/LoginForm.tsx
+++ src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,9 +1,14 @@
 import { useFormik } from "formik";
-import { Button, Form } from "semantic-ui-react";
+import * as Yup from "yup";
+import { Button, Form, Icon } from "semantic-ui-react";
+import IconPopup from "../../IconPopup";
 import { AuthInput } from "../../../__generated__/graphql";
 import "./LoginForm.scss";
+import { useState } from "react";
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const initialValues: AuthInput = {
     email: "",
     password: "",
@@ -11,11 +16,20 @@ const LoginForm = () => {
 
   const formik = useFormik({
     initialValues,
-    validationSchema: null,
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("This email is not valid")
+        .required("Email is required"),
+      password: Yup.string().required("Password is required"),
+    }),
     onSubmit: (values) => {
       console.log(values);
     },
   });
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <>
       <h2 className="login-form-title">
@@ -29,14 +43,35 @@ const LoginForm = () => {
           autoComplete="email"
           value={formik.values.email}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && !!formik.errors.email}
+          icon={
+            formik.touched.email &&
+            !!formik.errors.email && <IconPopup message={formik.errors.email} />
+          }
         />
         <Form.Input
-          type="password"
+          type={showPassword ? "text" : "password"}
           name="password"
           placeholder="Password"
           autoComplete="password"
           value={formik.values.password}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && !!formik.errors.password}
+          icon={
+            <div className="container-icon-password">
+              {formik.touched.password && !!formik.errors.password && (
+                <IconPopup message={formik.errors.password} />
+              )}
+              <Icon
+                aria-label="Toggle password visibility"
+                name={showPassword ? "eye slash" : "eye"}
+                link
+                onClick={toggleShowPassword}
+              />
+            </div>
+          }
         />
         <Button type="submit" className="btn-submit">
           Log In
