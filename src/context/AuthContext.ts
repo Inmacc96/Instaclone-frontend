3619d46d8fd7ba6ce41938c8f018a653183064fb
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-31 13:18:48 +0200

    Fix the type of setUser function

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -3,7 +3,7 @@ import { DecodedToken } from "../types/auth";
 
 interface AuthContextProps {
   auth: DecodedToken | null;
-  setUser: (user: DecodedToken) => void;
+  setUser: (user: DecodedToken | null) => void;
 }
 
 const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
