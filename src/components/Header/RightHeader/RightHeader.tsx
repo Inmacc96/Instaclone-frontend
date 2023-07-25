cc22d4921b822ae3525579579a501b42299f367e
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-07-25 20:37:53 +0200

    Navigate to the logged-in user's profile

--- src/components/Header/RightHeader/RightHeader.tsx
+++ src/components/Header/RightHeader/RightHeader.tsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom";
 import { Icon, Image } from "semantic-ui-react";
 import ImageNoFound from "../../../assets/png/avatar.png";
 import "./RightHeader.scss";
+import useAuth from "../../../hooks/useAuth";
 
 const RightHeader = () => {
+  const { auth } = useAuth();
   return (
     <div className="right-header">
       <Link to="/">
         <Icon name="home" />
       </Link>
       <Icon name="plus" />
-      <Link to="/">
+      <Link to={`/${auth?.username}`}>
         <Image src={ImageNoFound} avatar />
       </Link>
     </div>
