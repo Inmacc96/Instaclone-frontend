116661b61c2a9e3b614ff768ff4e931f87f1cb46
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-09-09 12:01:23 +0200

    Add react-dropzone to upload files

--- src/components/User/AvatarForm/AvatarForm.tsx
+++ src/components/User/AvatarForm/AvatarForm.tsx
@@ -1,15 +1,34 @@
+import { useCallback } from "react";
 import { Button } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
 import "./AvatarForm.scss";
+import { useQuery } from "@apollo/client";
+import { GENERATE_UPLOAD_URL } from "../../../gql/user";
 
 interface IAvatarForm {
   setShowModal: (v: boolean) => void;
 }
 const AvatarForm = ({ setShowModal }: IAvatarForm) => {
+
+  // Esta funcion siempre sera la misma entre renderizados, la funcion no se recreará
+  const onDrop = useCallback((acceptedFile: File[]) => {
+    const file = acceptedFile[0];
+
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "image/jpeg": [".jpeg"], "image/png": [".png"] },
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
+
   return (
     <div className="avatar-form">
-      <Button>Cargar una foto</Button>
+      <Button {...(getRootProps() as any)}>Cargar una foto</Button>
       <Button>Eliminar foto actual</Button>
       <Button onClick={() => setShowModal(false)}>Cancelar</Button>
+      <input {...getInputProps()} />
     </div>
   );
 };
