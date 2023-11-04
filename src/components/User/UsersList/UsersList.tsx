61ebc927fe8a4e82d2bf1bdfc29005e2c0b5534c
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-11-04 17:58:36 +0100

    Redirect to user profile when clicking on this user

--- src/components/User/UsersList/UsersList.tsx
+++ src/components/User/UsersList/UsersList.tsx
@@ -2,6 +2,7 @@ import { Image } from "semantic-ui-react";
 import { User } from "../../../__generated__/graphql";
 import ImageNoFound from "../../../assets/png/avatar.png";
 import "./UsersList.scss";
+import { useNavigate } from "react-router-dom";
 
 interface UserListProps {
   users: Pick<User, "id" | "username" | "name" | "avatar">[];
@@ -9,13 +10,21 @@ interface UserListProps {
 }
 
 const UsersList = ({ users, setShowModal }: UserListProps) => {
+  const navigate = useNavigate();
   return (
     <div className="users-list">
       {users.length === 0 ? (
         <p className="users-list__not-users">No se han encontrado usuarios</p>
       ) : (
         users.map((user) => (
-          <div key={user.id} className="users-list__user">
+          <div
+            key={user.id}
+            className="users-list__user"
+            onClick={() => {
+              setShowModal(false);
+              navigate(`/${user.username}`);
+            }}
+          >
             <Image src={user.avatar ?? ImageNoFound} avatar />
             <div>
               <p>{user.name}</p>
