c78a11baebbf63a6c58f5fe6a447f24d5d0d408b
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-11-04 17:16:31 +0100

    Add a modal when clicking on followers

--- src/components/User/Profile/Followers/Followers.tsx
+++ src/components/User/Profile/Followers/Followers.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_FOLLOWERS } from "../../../../gql/follow";
+import ModalBasic from "../../../Modal/ModalBasic";
 import "./Followers.scss";
 
 interface FollowersProps {
@@ -7,6 +9,9 @@ interface FollowersProps {
 }
 
 const Followers = ({ username }: FollowersProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+  const [childrenModal, setChildrenModal] = useState<JSX.Element | null>(null);
   const { data: dataFollowers, loading: loadingFollowers } = useQuery(
     GET_FOLLOWERS,
     {
@@ -14,21 +19,36 @@ const Followers = ({ username }: FollowersProps) => {
     }
   );
 
+  const openFollowers = () => {
+    setTitleModal("Followers");
+    setShowModal(true);
+    setChildrenModal(
+      <div>
+        <h3>Lista de seguidore</h3>
+      </div>
+    );
+  };
+
   if (loadingFollowers) return null;
 
   const { getFollowers } = dataFollowers!;
   return (
-    <div className="followers">
-      <p>
-        <span>50</span> posts
-      </p>
-      <p className="link">
-        <span>{getFollowers.length}</span> followers
-      </p>
-      <p className="link">
-        <span>240</span> followings
-      </p>
-    </div>
+    <>
+      <div className="followers">
+        <p>
+          <span>50</span> posts
+        </p>
+        <p className="link" onClick={openFollowers}>
+          <span>{getFollowers.length}</span> followers
+        </p>
+        <p className="link">
+          <span>240</span> followings
+        </p>
+      </div>
+      <ModalBasic show={showModal} setShow={setShowModal} title={titleModal}>
+        {childrenModal}
+      </ModalBasic>
+    </>
   );
 };
 
