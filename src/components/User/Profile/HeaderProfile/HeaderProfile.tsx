041c563dfbfad8120ad71a838e071f32ac35d6c6
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-11-05 19:11:27 +0100

    Update cache when follow/unfollow a user

--- src/components/User/Profile/HeaderProfile/HeaderProfile.tsx
+++ src/components/User/Profile/HeaderProfile/HeaderProfile.tsx
@@ -4,10 +4,13 @@ import { ApolloError, useMutation, useQuery } from "@apollo/client";
 import useAuth from "../../../../hooks/useAuth";
 import {
   FOLLOW_USER,
+  GET_FOLLOWERS,
+  GET_FOLLOWINGS,
   IS_FOLLOWING,
   UNFOLLOW_USER,
 } from "../../../../gql/follow";
 import "./HeaderProfile.scss";
+import { GET_USER } from "../../../../gql/user";
 
 interface HeaderProfileProps {
   username: string;
@@ -21,6 +24,7 @@ const HeaderProfile = ({ username, handlerModal }: HeaderProfileProps) => {
   });
   const [followUser] = useMutation(FOLLOW_USER, {
     update(cache) {
+      // Actualizar isFollowing
       cache.writeQuery({
         query: IS_FOLLOWING,
         variables: { username },
@@ -28,6 +32,52 @@ const HeaderProfile = ({ username, handlerModal }: HeaderProfileProps) => {
           isFollowing: true,
         },
       });
+
+      // Actualizar getFollowers
+      const getFollowersQuery = cache.readQuery({
+        query: GET_FOLLOWERS,
+        variables: { username },
+      });
+      const getAuthUserQuery = cache.readQuery({
+        query: GET_USER,
+        variables: { username: auth?.username },
+      });
+
+      if (getFollowersQuery?.getFollowers && getAuthUserQuery?.getUser) {
+        cache.writeQuery({
+          query: GET_FOLLOWERS,
+          variables: { username },
+          data: {
+            getFollowers: [
+              ...getFollowersQuery?.getFollowers,
+              getAuthUserQuery?.getUser,
+            ],
+          },
+        });
+      }
+
+      // Actualizar getFollowings
+      const getFollowingsQuery = cache.readQuery({
+        query: GET_FOLLOWINGS,
+        variables: { username: auth?.username ?? "" },
+      });
+      const getCurrentUserQuery = cache.readQuery({
+        query: GET_USER,
+        variables: { username },
+      });
+
+      if (getFollowingsQuery?.getFollowings && getCurrentUserQuery?.getUser) {
+        cache.writeQuery({
+          query: GET_FOLLOWINGS,
+          variables: { username: auth?.username ?? "" },
+          data: {
+            getFollowings: [
+              ...getFollowingsQuery?.getFollowings,
+              getCurrentUserQuery?.getUser,
+            ],
+          },
+        });
+      }
     },
   });
 
@@ -40,6 +90,50 @@ const HeaderProfile = ({ username, handlerModal }: HeaderProfileProps) => {
           isFollowing: false,
         },
       });
+
+      // Actualizar getFollowers
+      const getFollowersQuery = cache.readQuery({
+        query: GET_FOLLOWERS,
+        variables: { username },
+      });
+      const getAuthUserQuery = cache.readQuery({
+        query: GET_USER,
+        variables: { username: auth?.username },
+      });
+
+      if (getFollowersQuery?.getFollowers && getAuthUserQuery?.getUser) {
+        cache.writeQuery({
+          query: GET_FOLLOWERS,
+          variables: { username },
+          data: {
+            getFollowers: getFollowersQuery.getFollowers.filter(
+              (user) => user.id !== getAuthUserQuery.getUser.id
+            ),
+          },
+        });
+      }
+
+      // Actualizar getFollowings
+      const getFollowingsQuery = cache.readQuery({
+        query: GET_FOLLOWINGS,
+        variables: { username: auth?.username ?? "" },
+      });
+      const getCurrentUserQuery = cache.readQuery({
+        query: GET_USER,
+        variables: { username },
+      });
+
+      if (getFollowingsQuery?.getFollowings && getCurrentUserQuery?.getUser) {
+        cache.writeQuery({
+          query: GET_FOLLOWINGS,
+          variables: { username: auth?.username ?? "" },
+          data: {
+            getFollowings: getFollowingsQuery.getFollowings.filter(
+              (user) => user.id !== getCurrentUserQuery.getUser.id
+            ),
+          },
+        });
+      }
     },
   });
 
