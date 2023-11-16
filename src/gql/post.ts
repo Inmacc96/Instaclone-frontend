daa372a8e1a0cd4aff19ace046d274f18186983a
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-11-16 20:40:31 +0100

    Update post queries

--- src/gql/post.ts
+++ src/gql/post.ts
@@ -4,10 +4,7 @@ export const PUBLISH = gql(`#graphql
     mutation publish($urlFile: String!, $typeFile: String!) {
         publish(urlFile: $urlFile, typeFile: $typeFile) {
             id
-            idUser
             urlFile
-            typeFile
-            createdAt
         }
     }
 `);
@@ -16,10 +13,7 @@ export const GET_POSTS = gql(`#graphql
     query getPosts($username: String!){
         getPosts(username: $username) {
             id
-            idUser
             urlFile
-            typeFile
-            createdAt
         }
     }
 `);
