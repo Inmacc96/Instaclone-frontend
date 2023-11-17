9830ebdaa78d3201717a3143ca5af3e2b058fac3
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-11-17 21:17:19 +0100

    Wait for the like/dislike mutation to finish before re-launching onaction.

--- src/components/Modal/ModalPost/Actions/Actions.tsx
+++ src/components/Modal/ModalPost/Actions/Actions.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Icon } from "semantic-ui-react";
 import { toast } from "react-toastify";
 import { ApolloError, useMutation, useQuery } from "@apollo/client";
@@ -9,6 +10,7 @@ interface IActionsProps {
 }
 
 const Actions = ({ idPost }: IActionsProps) => {
+  const [isLoadingAction, setIsLoadingAction] = useState(false);
   const { loading, error, data } = useQuery(IS_LIKE, { variables: { idPost } });
   const {
     loading: loadingCountLikes,
@@ -75,20 +77,32 @@ const Actions = ({ idPost }: IActionsProps) => {
   });
 
   const onLike = async () => {
+    setIsLoadingAction(true);
     try {
       await like({ variables: { idPost } });
     } catch (error) {
       const err = error as ApolloError;
       toast.error(err.message);
+    } finally {
+      setIsLoadingAction(false);
     }
   };
 
   const onDislike = async () => {
+    setIsLoadingAction(true);
     try {
       await dislike({ variables: { idPost } });
     } catch (error) {
       const err = error as ApolloError;
       toast.error(err.message);
+    } finally {
+      setIsLoadingAction(false);
+    }
+  };
+
+  const onAction = () => {
+    if (!isLoadingAction) {
+      isLike ? onDislike() : onLike();
     }
   };
 
@@ -102,7 +116,7 @@ const Actions = ({ idPost }: IActionsProps) => {
       <Icon
         className={isLike ? "like active" : "like"}
         name={isLike ? "heart" : "heart outline"}
-        onClick={isLike ? onDislike : onLike}
+        onClick={onAction}
       />
       {countLikes} {countLikes === 1 ? "like" : "likes"}
     </div>
